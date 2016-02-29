bb0dfcbe128fe8609f3e94ebf9d9bacd20f6968d
Aaron Vincent Zabala <avgz@Aaron-Mac-Mini.local>
2016-02-29 18:38:54 +0800

    Initial Checkin

---
+++ PayList/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PayList
+//
+//  Created by Aaron Vincent Zabala on 29/02/2016.
+//  Copyright © 2016 Aaron Vincent Zabala. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
